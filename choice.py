d36f95312376df99e4e3cb7e7363a488479c7791
Kittiguya <157630166+Kittiguya@users.noreply.github.com>
2024-04-05 09:28:11 -0500

    converted to switch statements

--- choice.py
+++ choice.py
@@ -3,7 +3,7 @@ import random
 
 # making a random choice chooser between wow and rust. 
 
-# SOD phase 3 released today, Best Force Wipe update was released today in recent timing.
+# SOD phase 3 released today, Best Rust Force Wipe update was released today in recent timing.
 
 # i wanna play both but i lack the time. 
 
@@ -12,24 +12,29 @@ def choice(x, y):
     randomTask = random.choice(["dungeon-grind", "quest"])
     randomServer = random.choice(["Reddit", "Rustified", "Rusticated", "Rustoria", "Rusty Moose"])
 
-    if randomChoice == 'rust':
-        print("Rust")
-        if randomServer == "Reddit":
-            return "Reddit"
-        elif randomServer == "Rustified":
-            return "Rustified"
-        elif randomServer == "Rusticated":
-            return "Rusticated"
-        elif randomServer == "Rustoria":
-            return "Rustoria"
-        elif randomServer == "Rusty Moose":
-            return "Rusty Moose"
-    elif randomChoice == 'wow':
-        print("WoW")
-        if randomTask == "dungeon-grind":
-            return "Dungeon-Grind"
-        else:
-            return "Questing"
+    match randomChoice:
+        case "rust":
+            print("Rust")
+            match randomServer:
+                case "Reddit":
+                    return "Reddit"
+                case "Rustified":
+                        return "Rustified"
+                case "Rusticated":
+                        return "Rusticated"
+                case "Rustoria":
+                        return "Rustoria"
+                case "Rusty Moose":
+                        return "Rusty Moose"
+            
+        case "wow":
+            print("wow")
+            match randomTask:
+                case "dungeon-grind":
+                    return "Dungeon-Grind"
+                case "quest":
+                    return "Quest"
+
 
 print(choice("rust", "wow"))
 
